755fe57244774c2e726ca05a67a3327390145ae1
jihyung-hong <gbro@toss.im>
2019-02-21 16:30:37 +0900

    update test

--- src/main/java/study/effectivejava/chapter3/item10/Item10.java
+++ src/main/java/study/effectivejava/chapter3/item10/Item10.java
@@ -1,8 +1,6 @@
 package study.effectivejava.chapter3.item10;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +8,15 @@ import java.util.List;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * equals 는 일반 규약을 지켜 재정의해라
  */
+@Slf4j
 public class Item10 {
-    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Test
     public void p55() {
@@ -30,5 +32,45 @@ public class Item10 {
 
         assertTrue(list.contains(cis));
         assertFalse(list.contains(s));
+    }
+
+    @AllArgsConstructor
+    class Tt {
+        String aa;
+        int bb;
+
+    }
+
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    class Toverride {
+        String aa;
+        int bb;
+
+    }
+
+    @Test
+    public void 해시코드_equals_재정의_안한_case() {
+        Tt t1 = new Tt("aaa", 11);
+        Tt t2 = new Tt("aaa", 11);
+
+        log.info("t1:{}, t2:{}", t1.hashCode(), t2.hashCode());
+        log.info("t1 == t2 {}", t1 == t2);
+        log.info("t1.equals(t2) {}", t1.equals(t2));
+    }
+
+    @Test
+    public void 해시코드_equals_재정의_case() {
+        Toverride t1 = new Toverride("aaa", 11);
+        Toverride t2 = new Toverride("aaa", 11);
+
+        log.info("t1:{}, t2:{}", t1.hashCode(), t2.hashCode());
+        log.info("t1 == t2 {}", t1 == t2);
+        log.info("t1.equals(t2) {}", t1.equals(t2));
+    }
+
+    @Test
+    public void tt() {
+
     }
 }
